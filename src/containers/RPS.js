838d5baea4d69986e67a1ed475f6d8443e394cb0
Andres Fernando <andres.fernando08@gmail.com>
2019-11-15 20:16:51 -0500

    imagenes representativas añadidas

--- src/containers/RPS.js
+++ src/containers/RPS.js
@@ -3,8 +3,13 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
 import classes from './RPS.module.css'
+
+import ic_rock from '../images/ic_rock.png'
+import ic_paper from '../images/ic_paper.png'
+import ic_scissor from '../images/ic_scissor.png'
 
 export default class RPS extends React.Component {
     
@@ -13,9 +18,36 @@ export default class RPS extends React.Component {
         return (
             <div className={classes.RPS}>
                 <Container maxWidth='md'>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Paper className={classes.paperCustom}>xs 12</Paper>
+                    <Grid container direction='row' justify='space-around' alignItems='center'>
+                        <Grid item xs={3} container direction='column'>
+                            <Grid item xs={8}>
+                                <Paper className={classes.paperCustom}>
+                                    <img src={ic_rock} alt='Rock' title='Choose the Rock' />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button variant='contained' color='secondary'>Rock</Button>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={3} container direction='column'>
+                            <Grid item xs={8}>
+                                <Paper className={classes.paperCustom}>
+                                    <img src={ic_paper} alt='Paper' title='Choose the Paper' />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button variant='contained' color='secondary'>Paper</Button>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={3} container direction='column'>
+                            <Grid item xs={8}>
+                                <Paper className={classes.paperCustom}>
+                                    <img src={ic_scissor} alt='Scissor' title='Choose the Scissor' />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button variant='contained' color='secondary'>Scissor</Button>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Container>
