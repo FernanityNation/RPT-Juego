0675866cfb2b713fd28e4fbc7057823a15687aea
Andres Fernando <andres.fernando08@gmail.com>
2019-11-17 13:00:54 -0500

    utilizado los ajustes del commit anterior e implementado el componente del boton

--- src/containers/RPS.js
+++ src/containers/RPS.js
@@ -4,6 +4,7 @@ import classes from './RPS.module.css'
 
 import CardRow from '../UI/CardRow/CardRow'
 import CardItem from '../UI/CardItem/CardItem'
+import Button from '../UI/Button/Button'
 
 import ic_rock from '../images/ic_rock.png'
 import ic_paper from '../images/ic_paper.png'
@@ -15,13 +16,16 @@ export default class RPS extends React.Component {
         return (
             <div className={classes.RPS}>
                 <CardRow>
-                    <CardItem>
+                    <CardItem 
+                        cardAction={<Button>Rock</Button>}>
                         <img src={ic_rock} alt='Rock' title='Choose the Rock' />
                     </CardItem>
-                    <CardItem>
+                    <CardItem
+                        cardAction={<Button>Paper</Button>}>
                         <img src={ic_paper} alt='Paper' title='Choose the Paper' />
                     </CardItem>
-                    <CardItem>
+                    <CardItem
+                        cardAction={<Button>Scissor</Button>}>
                         <img src={ic_scissor} alt='Scissor' title='Choose the Scissor' />
                     </CardItem>
                 </CardRow>
