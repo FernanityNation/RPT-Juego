95f0aac570c1d04b551f0125af1a2641db5cc3f6
Andres Fernando <andres.fernando08@gmail.com>
2019-11-17 12:44:53 -0500

    refactorizado el componente de los elementos de juego

--- src/UI/CardItem/CardItem.js
+++ src/UI/CardItem/CardItem.js
@@ -4,9 +4,13 @@ import classes from './CardItem.module.css'
 
 
 const cardItem = props => (
-    <div className={classes.CardItem}>
-        {props.children}
+    <div className={classes.cardOption}>
+        <div className={classes.CardItem}>
+            {props.children}
+        </div>
+        {props.cardAction}
     </div>
+    
 )
 
 export default cardItem
