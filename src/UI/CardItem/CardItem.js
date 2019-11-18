4b9b511e3225f9da5c2c50dc60b4243ed233d080
Andres Fernando <andres.fernando08@gmail.com>
2019-11-17 21:15:40 -0500

    Cada elemento se le agrego una clase css cuando es seleccionado

--- src/UI/CardItem/CardItem.js
+++ src/UI/CardItem/CardItem.js
@@ -3,14 +3,20 @@ import React from 'react'
 import classes from './CardItem.module.css'
 
 
-const cardItem = props => (
-    <div className={classes.cardOption}>
-        <div className={classes.CardItem}>
-            {props.children}
+const cardItem = props => {
+    let customClasses = [classes.CardItem]
+    if (props.selected)
+        customClasses.push(classes.CardSelected)
+
+
+    return (
+        <div className={classes.cardOption}>
+            <div className={customClasses.join(' ')}>
+                {props.children}
+            </div>
+            {props.cardAction}
         </div>
-        {props.cardAction}
-    </div>
-    
-)
+    )
+}
 
 export default cardItem
