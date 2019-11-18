01f9d5951b100818c61ea720cc759601268280aa
Andres Fernando <andres.fernando08@gmail.com>
2019-11-18 11:29:05 -0500

    capa translucida al elemento de juego

--- src/UI/CardItem/CardItem.js
+++ src/UI/CardItem/CardItem.js
@@ -5,12 +5,15 @@ import classes from './CardItem.module.css'
 
 const cardItem = props => {
     let customClasses = [classes.CardItem]
-    if (props.selected)
+    let overlayClasses = [classes.cardOverlay]
+    if (props.selected) {
         customClasses.push(classes.CardSelected)
-
+        overlayClasses.push(classes.activeOverlay)
+    }
 
     return (
         <div className={classes.cardOption}>
+            <div className={overlayClasses.join(' ')} style={{visibility: props.selected ? 'visible' : 'hidden'}}></div>
             <div className={customClasses.join(' ')}>
                 {props.children}
             </div>
